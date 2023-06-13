dafc8434a8027a9314dcda34c9929234b5eaecfb
lucastenani <lucas.tenani3@gmail.com>
2023-06-12 23:25:33 -0300

    Script that changes the image path according to focus

---
+++ challenges/intermediate/login-form-css/script.js
@@ -0,0 +1,21 @@
+const emailInput = document.getElementById("email");
+const emailIcon = document.getElementById("email-icon");
+
+const passwordInput = document.getElementById("password");
+const lockIcon = document.getElementById("lock-icon");
+
+passwordInput.addEventListener("focus", () => {
+  lockIcon.src = "./images/lock-focus.svg";
+});
+
+passwordInput.addEventListener("blur", () => {
+  lockIcon.src = "./images/lock.svg";
+});
+
+emailInput.addEventListener("focus", () => {
+  emailIcon.src = "./images/mail-focus.svg";
+});
+
+emailInput.addEventListener("blur", () => {
+  emailIcon.src = "./images/mail.svg";
+});
