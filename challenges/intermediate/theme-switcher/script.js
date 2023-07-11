55bd8bb99a8173f491dc19ca037812af229f9196
lucastenani <lucas.tenani3@gmail.com>
2023-07-11 19:17:58 -0300

    adding local storage

--- challenges/intermediate/theme-switcher/script.js
+++ challenges/intermediate/theme-switcher/script.js
@@ -1,8 +1,20 @@
 const checkButton = document.getElementById("checkbox");
 const body = document.querySelector("body").classList;
+const localStorageKey = "themeState";
+const savedState = localStorage.getItem(localStorageKey);
 
-checkButton.addEventListener("change", (e) => {
-  let isCheck = e.target.checked;
+if (savedState === "true") {
+  checkButton.checked = true;
+  body.add("dark");
+} else {
+  checkButton.checked = false;
+  body.remove("dark");
+}
+
+checkButton.addEventListener("change", () => {
+  let isCheck = checkButton.checked;
 
   isCheck ? body.add("dark") : body.remove("dark");
+
+  localStorage.setItem(localStorageKey, isCheck);
 });
