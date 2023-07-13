58c8cfc4e38bececeda19512007d1c4038438ffa
lucastenani <lucas.tenani3@gmail.com>
2023-07-13 19:19:04 -0300

    First section of main content completed

---
+++ challenges/advanced/nfts/script.js
@@ -0,0 +1,6 @@
+const menuButton = document.getElementById("menu-button");
+const sidebar = document.getElementById("sidebar");
+
+// menuButton.addEventListener("click", () => {
+//   sidebar.classList.toggle("open");
+// });
